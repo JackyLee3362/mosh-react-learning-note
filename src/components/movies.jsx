f15a775ed20be03b4db907e0cf2cb1e0f3a0d22a
Jacky <foo@bar.com>
2022-06-23 16:39:05 +0800

    add something

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -107,7 +107,7 @@ class Movies extends Component {
     } = this.state;
     const { user } = this.props;
 
-    if (count === 0) return <p>There are no movies in the database.</p>;
+    // if (count === 0) return <p>There are no movies in the database.</p>;
     const { totalCount, data: movies } = this.getPageDate();
     return (
       <React.Fragment>
